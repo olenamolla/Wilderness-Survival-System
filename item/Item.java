2267540123358baacd140dc86b2c3c0d9be9416d
Olena Molla <133937893+olenamolla@users.noreply.github.com>
2025-04-27 12:00:46 -0700

    feat: Add abstract Item class for map bonuses
    - Created Item abstract class with name and repeating attributes
    
    - Added abstract applyTo(Player) method to enforce bonus behavior in subclasses
    
    - Prepares structure for specific item types like FoodBonus, WaterBonus, and GoldBonus

--- item/Item.java
+++ item/Item.java
@@ -1,5 +1,55 @@
 package wss.item;
 
-public class Item {
+import wss.player.Player;
+
+/**
+ * Abstract base class representing an item that can be found on a map square.
+ * Items can affect the player by giving bonuses like food, water, or gold.
+ * Some items may be repeating, allowing the player to collect them multiple times.
+ */
+
+public abstract class Item {
     
+    /** Name of the item (e.g., "Food Bonus", "Water Bonus") */
+    protected String name;
+
+    /** Whether the item is repeating */
+    protected boolean repeating;
+
+    /**
+     * Constructs an item with a specified name and repeatability.
+     * 
+     * @param name The name of the item
+     * @param repeating True if the item can be collected multiple times, false if one-time only
+     */
+    public Item(String name, boolean repeating) {
+        this.name = name;
+        this.repeating = repeating;
+    }
+
+    /**
+     * Applies the effect of this item to the player.
+     * 
+     * Each subclass (FoodBonus, WaterBonus, GoldBonus) will implement how it affects the player.
+     *
+     * @param player The player who is collecting the item
+     */
+    public abstract void applyTo(Player player);
+
+
+    /**
+     * Returns the name of the item.
+     * @return Item name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Checks if the item is repeating.
+     * @return True if repeating, false otherwise
+     */
+    public boolean isRepeating() {
+        return repeating;
+    }
 }
