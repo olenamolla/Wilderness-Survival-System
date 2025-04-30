8fee1eb02e190330fdcbd6f8c68d43b958418eb2
Howard-GitHub <howard.l.general@gmail.com>
2025-04-29 21:07:07 -0700

    Update Vision code
    Forgot to save changes before pushing.

--- vision/CautiousVision.java
+++ vision/CautiousVision.java
@@ -1,10 +1,10 @@
 package wss.vision;
 
-import java.util.ArrayList;
 import wss.map.GameMap;
 import wss.player.Player;
 import wss.map.MapSquare;
 import wss.item.Item;
+import wss.util.Path;
 
 // CautiousVision scans only the adjacent squares (North, East, South). 
 // Prioritizes moving to squares that are close and safe. 
@@ -15,7 +15,6 @@ public class CautiousVision extends Vision {
     private List<MapSquare> squaresThatHaveGold;
     private List<MapSquare> squaresThatHaveWater;
     private List<MapSquare> squaresThatHaveFood;
-    private int[][] mapSquarePositions;
     private int[] currentPosition;
 
     public CautiousVision() {
@@ -23,40 +22,59 @@ public class CautiousVision extends Vision {
         squaresThatHaveFood = new ArrayList<>();
         squaresThatHaveGold = new ArrayList<>();
         squaresThatHaveFood = new ArrayList<>();
-        mapSquarePositions = new int[3][2];
         currentPosition = player.getPosition();     // getPosition is not an available function yet
     }
 
-    public boolean hasFood(List<Item> items) {
+    /**
+     * Checks if a map square has food
+     * 
+     * @param items The list of items in a map square
+     */
+    private boolean hasFood(List<Item> items) {
         for (int index = 0; index  < items.size(); index++) {
             Item item = items.get(index);
-            if (item.getName().i)
+            if (item.getName().equals("Food bonus")) {
+                return true;
+            }
         }
+        return false;
     }
 
-    public PlayerPath closestFood() {
+    /**
+     * Returns the path to the closest food. If there are more than one
+     * path that is the same distance, this method chooses the path that
+     * leads to more food. If there are multiple paths that are the same
+     * distance and have the same amount of food, choose path randomly.
+     * 
+     * @return Path object containing the best path to food
+     */
+    public Path closestFood() {
         for (int index = 0; index < visibleSquares.size(); index++) {
-            List<Item> items = visibleSquares.get(index).getItems();
-
+            MapSquare currentSquare = visibleSquares.get(index);
+            List<Item> items = currentSquare.getItems();
+            if (hasFood(items, currentSquare)) {
+                squaresThatHaveFood.add(currentSquare);
+            }
         }
+
+        /* to do: need to create algorithm to determine which square to choose
+         * if there are multiple paths with the same distance
+        */
+        
+
     }
 
+    /**
+     * Retrieves all visible squares
+     * 
+     * @param map The map of the entire game
+     * @param player The player in the game
+     * @return List of map squares visible the the player
+     */
     public List<MapSquare> getVisibleSquares(GameMap map, Player player) {
-
-        // storing north square and its position
         visibleSquares.add(map.getSquare(currentPosition[0], currentPosition[1] + 1));
-        mapSquarePositions[0][0] = currentPosition[0];
-        mapSquarePositions[0][1] = currentPosition[1] + 1;
-
-        // storing east square and its position
         visibleSquares.add(map.getSquare(currentPosition[0] + 1, currentPosition[1]));
-        mapSquarePositions[1][0] = currentPosition[0] + 1;
-        mapSquarePositions[1][1] = currentPosition[1];
-
-        // storing south square and its position
         visibleSquares.add(map.getSquare(currentPosition[0], currentPosition[1] - 1));
-        mapSquarePositions[2][0] = currentPosition[0];
-        mapSquarePositions[2][1] = currentPosition[1] - 1;
 
         return visibleSquares;
     }
--- vision/Vision.java
+++ vision/Vision.java
@@ -1,14 +1,77 @@
 package wss.vision;
 
+import java.util.ArrayList;
 import wss.map.GameMap;
 import wss.player.Player;
 import wss.map.MapSquare;
 import wss.item.Item;
+import wss.util.Path;
 
 
 public abstract class Vision {
-    public abstract PlayerPath closestFood();
-    public abstract PlayerPath closestWater();
-    public abstract PlayerPath easiestPath();
+    private List<MapSquare> visibleSquares;
+    private List<MapSquare> squaresThatHaveGold;
+    private List<MapSquare> squaresThatHaveWater;
+    private List<MapSquare> squaresThatHaveFood;
+    private int[] currentPosition;
+
+    /**
+     * Checks if a map square has food
+     * 
+     * @param items The list of items in a map square
+     */
+    protected abstract boolean hasFood(List<Item> items);
+
+    /**
+     * Checks if a map square has water
+     * 
+     * @param items The list of items in a map square
+     */
+    protected abstract boolean hasWater(List<Item> items);
+
+    /**
+     * Checks if a map square has gold
+     * 
+     * @param items The list of items in a map square
+     * @return Does the square contain gold?
+     */
+    protected abstract boolean hasGold(List<Item> items);
+
+    /**
+     * Returns the path to the closest food. If there are more than one
+     * path that is the same distance, this method chooses the path that
+     * leads to more food. If there are multiple paths that are the same
+     * distance and have the same amount of food, choose path randomly.
+     * 
+     * @return Path object containing the best path to food
+     */
+    public abstract Path closestFood();
+
+    /**
+     * Returns the path to the closest water. If there are more than one
+     * path that is the same distance, this method chooses the path that
+     * leads to more water. If there are multiple paths that are the same
+     * distance and have the same amount of water, choose path randomly.
+     * 
+     * @return Path object containing the best path to water
+     */
+    public abstract Path closestWater();
+
+    /**
+     * Returns the path that has the least cost overall. Determines least
+     * cost by summing all costs together and choosing the sum that is
+     * the least.
+     * 
+     * @return Path object containing the easiest path
+     */
+    public abstract Path easiestPath();
+
+    /**
+     * Retrieves all visible squares
+     * 
+     * @param map The map of the entire game
+     * @param player The player in the game
+     * @return List of map squares visible the the player
+     */
     public abstract List<MapSquare> getVisibleSquares(GameMap map, Player player);
 }
