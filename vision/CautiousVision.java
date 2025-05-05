69b3b23fac2b3c04d85fcb5ac9b9b6ad402e4bd5
Howard-GitHub <howard.l.general@gmail.com>
2025-05-05 00:01:47 -0700

    Update vision classes

--- vision/CautiousVision.java
+++ vision/CautiousVision.java
@@ -1,12 +1,12 @@
 package wss.vision;
 
-import wss.map.GameMap;
-import wss.player.Player;
-import wss.map.MapSquare;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import wss.map.GameMap;
+import wss.map.Terrain;
+import wss.player.Player;
+import wss.map.MapSquare;
 import wss.item.Item;
 import wss.util.Path;
 import wss.player.MoveDirection;
@@ -16,18 +16,11 @@ import wss.player.MoveDirection;
 // Avoids expensive or risky paths.
 public class CautiousVision extends Vision {
 
-    private List<MapSquare> visibleSquares;
-    private List<MoveDirection> directions;
-    private List<MapSquare> squaresThatHaveGold;
-    private List<MapSquare> squaresThatHaveWater;
-    private List<MapSquare> squaresThatHaveFood;
+    protected List<MoveDirection> directions;
 
     public CautiousVision() {
-        visibleSquares = new ArrayList<>();
-        directions = new ArrayList<>();
-        squaresThatHaveFood = new ArrayList<>();
-        squaresThatHaveGold = new ArrayList<>();
-        squaresThatHaveFood = new ArrayList<>();
+        super();
+        this.directions = new ArrayList<>();
     }
 
     private boolean hasItem(MapSquare currentSquare) {
@@ -55,12 +48,14 @@ public class CautiousVision extends Vision {
         // the closest and best path to the square with food
         Path optimalPath = null;
 
+        MapSquare currentSquare;
+
         // iterates over each visible square to look for the optimal path
         for (int index = 0; index < visibleSquares.size(); index++) {
-            MapSquare currentSquare = visibleSquares.get(index);
+            currentSquare = visibleSquares.get(index);
             
             // if the current square has food, then evaluate whether it is the optimal path
-            if (hasItem(currentSquare)) {    // replace hasItem() with MapSquare function once it exists
+            if (hasFood(currentSquare)) {    // replace hasFood() with MapSquare function once it exists
                 currentPath = new Path();
                 currentPath.addStep(directions.get(index), currentSquare.getTerrain());
                 currentTotalCost = currentPath.getMovementCost() + currentPath.getFoodCost() + currentPath.getWaterCost();
@@ -77,6 +72,103 @@ public class CautiousVision extends Vision {
     }
 
     /**
+     * Returns the path to the closest water. If there are more than one
+     * path that is the same distance, this method chooses the path that
+     * leads to more water. If there are multiple paths that are the same
+     * distance and have the same amount of water, choose path randomly.
+     * 
+     * @return Path object containing the best path to water
+     */
+    public Path closestWater() {
+        // contains the square that will take the least accumulated cost (movement + gold + water) to get to
+        int bestTotalCost = Integer.MAX_VALUE;
+
+        // contains the current total cost of the iteration
+        int currentTotalCost = Integer.MAX_VALUE;
+
+        // the current path of the iteration
+        Path currentPath = null;
+
+        // the closest and best path to the square with food
+        Path optimalPath = null;
+
+        MapSquare currentSquare;
+
+        // iterates over each visible square to look for the optimal path
+        for (int index = 0; index < visibleSquares.size(); index++) {
+            currentSquare = visibleSquares.get(index);
+            
+            // if the current square has food, then evaluate whether it is the optimal path
+            if (hasWater(currentSquare)) {    // replace hasWater() with MapSquare function once it exists
+                currentPath = new Path();
+                currentPath.addStep(directions.get(index), currentSquare.getTerrain());
+                currentTotalCost = currentPath.getMovementCost() + currentPath.getFoodCost() + currentPath.getWaterCost();
+                
+                // set the new optimal path if a better path is found
+                if (currentTotalCost < bestTotalCost) {
+                    bestTotalCost = currentTotalCost;
+                    optimalPath = currentPath;
+                }
+            }
+        }
+
+        return optimalPath;
+    }
+
+    /**
+     * Returns the path to the closest gold. If there are more than one
+     * path that is the same distance, this method chooses the path that
+     * leads to more gold. If there are multiple paths that are the same
+     * distance and have the same amount of gold, choose path randomly.
+     * 
+     * @return Path object containing the best path to gold
+     */
+    public Path closestGold() {
+        // contains the square that will take the least accumulated cost (movement + gold + water) to get to
+        int bestTotalCost = Integer.MAX_VALUE;
+
+        // contains the current total cost of the iteration
+        int currentTotalCost = Integer.MAX_VALUE;
+
+        // the current path of the iteration
+        Path currentPath = null;
+
+        // the closest and best path to the square with food
+        Path optimalPath = null;
+
+        MapSquare currentSquare;
+
+        // iterates over each visible square to look for the optimal path
+        for (int index = 0; index < visibleSquares.size(); index++) {
+            currentSquare = visibleSquares.get(index);
+            
+            // if the current square has food, then evaluate whether it is the optimal path
+            if (hasGold(currentSquare)) {    // replace hasGold() with MapSquare function once it exists
+                currentPath = new Path();
+                currentPath.addStep(directions.get(index), currentSquare.getTerrain());
+                currentTotalCost = currentPath.getMovementCost() + currentPath.getFoodCost() + currentPath.getWaterCost();
+                
+                // set the new optimal path if a better path is found
+                if (currentTotalCost < bestTotalCost) {
+                    bestTotalCost = currentTotalCost;
+                    optimalPath = currentPath;
+                }
+            }
+        }
+
+        return optimalPath;
+    }
+
+    /**
+     * Stores the squares that are only one square away from the player
+     * for later evaluation to determine which square has the least cost
+     */
+    protected void setEasiestSquares() {
+        easiestSquares.add(visibleSquares.get(1));
+        easiestSquaresDirections.add(MoveDirection.EAST);
+    }
+
+    /**
      * Retrieves all visible squares
      * 
      * @param map The map of the entire game
@@ -84,18 +176,17 @@ public class CautiousVision extends Vision {
      * @return List of map squares visible the the player
      */
     public List<MapSquare> getVisibleSquares(GameMap map, Player player) {
-        int[] currentPosition = player.getPosition();     // getPosition is not an available function yet
 
         // stores square and direction for north square
-        visibleSquares.add(map.getSquare(currentPosition[0], currentPosition[1] + 1));
+        visibleSquares.add(map.getSquare(player.getX(), player.getY() - 1));
         directions.add(MoveDirection.NORTH);
 
         // stores square and direction for east square
-        visibleSquares.add(map.getSquare(currentPosition[0] + 1, currentPosition[1]));
+        visibleSquares.add(map.getSquare(player.getX() + 1, player.getY()));
         directions.add(MoveDirection.EAST);
 
         // stores square and direction for south square
-        visibleSquares.add(map.getSquare(currentPosition[0], currentPosition[1] - 1));
+        visibleSquares.add(map.getSquare(player.getX(), player.getY() + 1));
         directions.add(MoveDirection.SOUTH);
 
         return visibleSquares;
--- vision/Vision.java
+++ vision/Vision.java
@@ -1,47 +1,26 @@
 package wss.vision;
 
-<<<<<<< HEAD
-=======
 import java.util.ArrayList;
 import java.util.List;
 
 import wss.map.GameMap;
+import wss.player.MoveDirection;
 import wss.player.Player;
 import wss.map.MapSquare;
 import wss.item.Item;
 import wss.util.Path;
 
 
->>>>>>> origin/main
 public abstract class Vision {
-    private List<MapSquare> visibleSquares;
-    private List<MoveDirection> directions;
-    private List<MapSquare> squaresThatHaveGold;
-    private List<MapSquare> squaresThatHaveWater;
-    private List<MapSquare> squaresThatHaveFood;
-    private int[] currentPosition;
 
-    /**
-     * Checks if a map square has food
-     * 
-     * @param items The list of items in a map square
-     */
-    protected abstract boolean hasFood(List<Item> items);
+    protected List<MapSquare> visibleSquares;
+    protected List<MapSquare> easiestSquares;
+    protected List<MoveDirection> easiestSquaresDirections;
 
-    /**
-     * Checks if a map square has water
-     * 
-     * @param items The list of items in a map square
-     */
-    protected abstract boolean hasWater(List<Item> items);
-
-    /**
-     * Checks if a map square has gold
-     * 
-     * @param items The list of items in a map square
-     * @return Does the square contain gold?
-     */
-    protected abstract boolean hasGold(List<Item> items);
+    public Vision() {
+        this.visibleSquares = new ArrayList<>();
+        this.easiestSquares = new ArrayList<>();
+    }
 
     /**
      * Returns the path to the closest food. If there are more than one
@@ -64,13 +43,61 @@ public abstract class Vision {
     public abstract Path closestWater();
 
     /**
-     * Returns the path that has the least cost overall. Determines least
-     * cost by summing all costs together and choosing the sum that is
-     * the least.
+     * Returns the path to the closest gold. If there are more than one
+     * path that is the same distance, this method chooses the path that
+     * leads to more gold. If there are multiple paths that are the same
+     * distance and have the same amount of gold, choose path randomly.
+     * 
+     * @return Path object containing the best path to gold
+     */
+    public abstract Path closestGold();
+
+
+    /**
+     * Returns a path in the direction of east that costs the 
+     * least overall. The path will only be one square long
      * 
      * @return Path object containing the easiest path
      */
-    public abstract Path easiestPath();
+    public Path easiestPath() {
+        // contains the square that will take the least accumulated cost (movement + gold + water) to get to
+        int bestTotalCost = Integer.MAX_VALUE;
+
+        // contains the current total cost of the iteration
+        int currentTotalCost = Integer.MAX_VALUE;
+
+        // the current path of the iteration
+        Path currentPath = null;
+
+        // the closest and best path to the square with food
+        Path optimalPath = null;
+
+        MapSquare currentSquare;
+
+        setEasiestSquares();
+
+        for (int index = 0; index < easiestSquares.size(); index++) {
+            currentSquare = visibleSquares.get(index);
+
+            currentPath = new Path();
+            currentPath.addStep(easiestSquaresDirections.get(index), currentSquare.getTerrain());
+            currentTotalCost = currentPath.getMovementCost() + currentPath.getFoodCost() + currentPath.getWaterCost();
+
+            // set the new optimal path if a better path is found
+            if (currentTotalCost < bestTotalCost) {
+                bestTotalCost = currentTotalCost;
+                optimalPath = currentPath;
+            }
+        }
+
+        return optimalPath;
+    }
+
+    /**
+     * Stores the squares that are only one square away from the player
+     * for later evaluation to determine which square has the least cost
+     */
+    protected abstract void setEasiestSquares();
 
     /**
      * Retrieves all visible squares
